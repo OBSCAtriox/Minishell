a5e876198f1a9aa4c7d48479083a8a82c55d9867
Tiago Alexandre De Paula Cruz <tide-pau@c2r8s11.42lisboa.com>
2026-01-12 18:47:40 +0000

    adding header to every parse file plus headers plus fixing norm write issue on headers since cant call a macro inside a macro

--- src/parse/redir/malloc_redir_array.c
+++
@@ -1,9 +0,0 @@
-#include "../../../includes/minishell.h"
-
-static int	count_redir(const char *li)
-{
-}
-
-int	malloc_redir_array(const char *li)
-{
-}
--- src/parse/verifications_after_malloc_struct.c
+++ src/parse/verifications_after_malloc_struct.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   verifications_after_malloc_struct.c                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tide-pau <tide-pau@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/12 18:37:05 by tide-pau          #+#    #+#             */
+/*   Updated: 2026/01/12 18:38:37 by tide-pau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../includes/minishell.h"
 
 int	verifications_after_malloc_struct(t_tokens *t)
